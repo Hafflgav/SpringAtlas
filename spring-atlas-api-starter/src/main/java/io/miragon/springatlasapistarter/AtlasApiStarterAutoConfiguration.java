9b7d8f64e880042bb84031cd1898effe459d24fc
Rami Nakkar <rami.nakkar@miragon.io>
2023-07-24 16:01:44 +0200

    feat: add example worker

--- spring-atlas-api-starter/src/main/java/io/miragon/springatlasapistarter/AtlasApiStarterAutoConfiguration.java
+++ spring-atlas-api-starter/src/main/java/io/miragon/springatlasapistarter/AtlasApiStarterAutoConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class AtlasApiStarterConfiguration {
+public class AtlasApiStarterAutoConfiguration {
 
     @Bean
     AnonymousSessionsApi anonymousSessionsApi(final ApiClient apiClient) {
